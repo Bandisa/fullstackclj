30ee73a2d8cb09cc52bbdb98a324abd21b693885
Bandisa <89698022+Bandisa@users.noreply.github.com>
2024-07-10 15:48:07 +0200

    Fix: using content-length to read the body and optimised handling socket input streams

--- src/fullstackclj/backend/helpers/http.clj
+++ src/fullstackclj/backend/helpers/http.clj
@@ -3,23 +3,19 @@
 
 (def method_regx #"(?i)(?:GET|POST|PUT|DELETE|PATCH|HEAD|OPTIONS|TRACE|CONNECT)")
 (def http_vers_regx #"(?i)(?:HTTP)\/\d\.\d")
-(def body_regx #"\r\n\r\n")
-(def crlf_regx #"\r\n")
 
 (defn parse
   "Parses an http request
    
+   request: a vector consisting of http request parameters
    returns: a map of http request elements"
   [request]
-  (let [lines (str/split request body_regx)
-        raw_req_header (str/split (first lines) crlf_regx)
-        req_line (first raw_req_header)
-
+  (let [req_line (first request)
         method (re-find method_regx req_line)
         url (second (str/split req_line #"\s"))
         http_vers (re-find http_vers_regx req_line)
-        headers (rest raw_req_header)
-        body (second lines)]
+        headers (second request)
+        body (peek request)]
     {:method method :url url :http_vers http_vers
      :headers headers
      :body body}))
@@ -31,5 +27,6 @@
         crlf "\r\n"
         headers (assoc headers "Content-Length" (count (or body "")))]
     (str http_vers space status_code space status_info crlf
-         (apply str (map #(str (key %) ": " (val %) crlf) headers)) crlf
+         (apply str (map #(str (key %) ": " (val %) crlf) headers))
+         crlf
          (if (nil? body) "" (str body crlf)))))
--- test/fullstackclj/backend/mock_data.clj
+++ test/fullstackclj/backend/mock_data.clj
@@ -1,10 +1,10 @@
 (ns fullstackclj.backend.mock-data)
 (def parser_test_data
-  {:case1 {:req "GET / http/1.1\r\na: s\r\nb: q\r\n\r\nbody"
+  {:case1 {:req ["GET / http/1.1" {"a: s", "b: q"} "body"]
            :res {:method "GET"
                  :url "/"
                  :http_vers "http/1.1"
-                 :headers ["a: s" "b: q"]
+                 :headers {"a: s", "b: q"}
                  :body "body"}}})
 
 (def response_test_data
